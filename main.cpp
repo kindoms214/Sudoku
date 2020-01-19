cfa2a9800acb02ff94d042c8632d94818f6b39a3
kindoms <43612318+kindoms214@users.noreply.github.com>
2020-01-19 10:14:06 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,34 @@
-#include"ProduceSoduku.h"
+#include "ProduceSudoku.h"
+#include "QuesGenerate.h"
+#include "SolveSudoku.h"
 
 int main(int argc, char *argv[])
-{
-			//int a;
-			//produce soduku;
-			//soduku.ProduceSudoku(1000000);
-	
-			if (strcmp(argv[1], "-c") == 0)
-			{	
-						produce soduku;
-						cout << "produce soduku" << endl;
-						if (soduku.AllisNum(argv[2]) == 0)	//非全数字
-						{
-										cout << "ERROR PARAMETER" << endl;
-										return 0;
-						}
-						soduku.ProduceSudoku(atoi(argv[2]));
-						//soduku.ProduceSudoku(100000);
-			}
-			else if (strcmp(argv[1], "-s") == 0)
-			{	
-						cout << "solve" << endl;
-						//SolveSudoku();
-			}
-	
-			//cin >> a;
+{	
+	if (strcmp(argv[1], "-c") == 0)
+	{
+		produce soduku;
+		cout << "produce soduku" << endl;
+		if (soduku.AllisNum(argv[2]) == 0)	//非全数字
+		{
+			cout << "ERROR PARAMETER" << endl;
 			return 0;
+		}
+		soduku.ProduceSudoku(atoi(argv[2]));
+	}
+	else if (strcmp(argv[1], "-s") == 0)
+	{
+		cout << "solve soduku" << endl;
+		Solve soduku;
+		soduku.SolveSudoku(argv[2]);
+		//SolveSudoku();
+	}
+	else if (strcmp(argv[1], "-g") == 0)
+	{
+		cout << "generate soduku" << endl;
+		generate_ques pro;
+		pro.QuesGenerate(atoi(argv[2]));
+	}
+
+	system("pause");
+	return 0;
 }
